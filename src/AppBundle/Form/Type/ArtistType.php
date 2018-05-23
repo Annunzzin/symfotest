3cbd7a96705778188e9453f65ed6cf8120e59269
formation <formation@formation-Latitude-7480.(none)>
2018-05-23 15:23:07 +0200

    some updates

--- src/AppBundle/Form/Type/ArtistType.php
+++ src/AppBundle/Form/Type/ArtistType.php
@@ -10,9 +10,12 @@ namespace AppBundle\Form\Type;
 
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArtistType extends AbstractType
@@ -20,9 +23,23 @@ class ArtistType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
+        /*$builder->addEventListener(FormEvents::PRE_SET_DATA,function(FormEvent $event){
+           $artist = $event->getData();
+           $form =  $event->getForm();
+
+           if($artist === null || $artist->getId() === null){
+               $form->add('type',TextType::class);
+           }
+        });*/
         $builder
             ->add('name',TextType::class)
-            ->add('type',TextType::class)
+            ->add('type',ChoiceType::class,[
+                'choices' => [
+                    'band' => 1,
+                    'solo' => 0
+                ]
+            ])
             ->add('picture',TextType::class)
             ->add('genre',TextType::class)
             ->add('submit',SubmitType::class);
